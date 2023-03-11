673cb07e50f832d09ea740bc1eef4ab8ec0fcedf
charlotte <charlottepaterson@outlook.com>
2023-03-11 23:08:24 +1300

    begin to implement product cards on homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,7 +3,7 @@
   <n-card class="banner">
   </n-card>
   <n-grid style="padding-top: 20px;" cols="3" item-responsive responsive="screen">
-    <n-grid-item style="padding: 20px 20px 20px 20px;">
+    <n-grid-item v-for="item in items" style="padding: 20px 20px 20px 20px;">
       <ShopItem/>
     </n-grid-item>
   </n-grid>
@@ -13,10 +13,18 @@
   import { defineComponent, h, ref, Component } from 'vue'
   import { NIcon, NGrid, NGridItem } from 'naive-ui'
   import ShopItem from './ShopItem.vue';
+  import products from '../assets/data/products.json';
+
+  const MAX_HOME_ITEMS = 6;
 
   export default defineComponent({
   components:{
     ShopItem
+  },
+  data() {
+    return {
+      items: products
+    }
   }
 })
 </script>
@@ -24,7 +32,7 @@
 <style scoped>
 .banner {
   max-width: 100%;
-  min-height: 400px;;
+  min-height: 600px;;
   background-color:black;
 }
 </style>
