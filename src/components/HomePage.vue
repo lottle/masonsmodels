8eccdca7626e674c79c98186a41220e774fecff9
lottle <charlottepaterson@outlook.com>
2023-03-12 15:02:00 +1300

    wip display product details in shop items on homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,30 +3,31 @@
   <n-card class="banner">
   </n-card>
   <n-grid style="padding-top: 20px;" cols="3" item-responsive responsive="screen">
-    <n-grid-item v-for="item in items" style="padding: 20px 20px 20px 20px;">
-      <ShopItem/>
+    <n-grid-item v-for="(product, index) in products" style="padding: 20px 20px 20px 20px;">
+      <ShopItem :title="product.name" :description="product.description" :img="product.img" :price="product.cost" />
     </n-grid-item>
+    <p></p>
   </n-grid>
 </template>
 
 <script lang="ts">
-  import { defineComponent, h, ref, Component } from 'vue'
-  import { NIcon, NGrid, NGridItem } from 'naive-ui'
+  import { defineComponent, h, ref, Component } from 'vue';
+  import { NIcon, NGrid, NGridItem } from 'naive-ui';
   import ShopItem from './ShopItem.vue';
-  import products from '../assets/data/products.json';
+  import productData from '../assets/data/products.json';
 
   const MAX_HOME_ITEMS = 6;
 
   export default defineComponent({
-  components:{
-    ShopItem
-  },
-  data() {
-    return {
-      items: products
+    components:{
+      ShopItem
+    },
+    data() {
+      return {
+        products: productData
+      }
     }
-  }
-})
+  })
 </script>
 
 <style scoped>
--- src/components/ShopItem.vue
+++ src/components/ShopItem.vue
@@ -1,14 +1,37 @@
 <template>
-    <n-card title="Card with Cover">
+    <n-card v-bind:title="title">
       <template #cover>
-        <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
+        <img v-bind:src="`../assets/products/${img}`">
       </template>
-      Card Content
+
+      {{ description }} 
+      <n-space >
+        <n-button>View</n-button>
+        <n-button type="primary">
+          Add to Cart
+        </n-button>
+        </n-space>
     </n-card>
   </template>
+
+<script>
+export default {
   
-  <style scoped>
-  .n-card {
-    max-width: 300px;
+  props: {
+    title: String,
+    description: String,
+    img: String,
+    price: String
   }
-  </style>
+}
+</script>  
+
+<style scoped>
+.n-card {
+  max-width: 300px;
+}
+.n-space {
+  padding-top: 10px;
+   text-align: center;
+}
+</style>
